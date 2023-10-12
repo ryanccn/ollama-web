5f34ce59b82c393a75f108ea386b3723f838abc5
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-10-12 14:46:56 +0800

    fix(deps): bump deps

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,9 @@ import router from './router';
 import { createHead } from '@unhead/vue';
 
 const app = createApp(App);
-const head = createHead();
 
 app.use(createPinia());
+app.use(createHead());
 app.use(router);
-app.use(head);
 
 app.mount('#app');
